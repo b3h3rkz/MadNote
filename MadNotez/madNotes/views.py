b0f5a40954909b380abf3306e88adba39f682a56
b3h3rkz <barjebernard@gmail.com>
2015-10-28 09:01:05 +0000

    added NoteView for adding new notes by the user

--- MadNotez/madNotes/views.py
+++ MadNotez/madNotes/views.py
@@ -1,11 +1,37 @@
+from django.http import request
 from django.views import generic
+from django.shortcuts import render, render_to_response, RequestContext
+from madNotes.models import Note, Tags
 from madNotes import models
+from .forms import NoteForm
+from django.views.generic.edit import FormView
+"""
+class NoteView(FormView):
+    template_name = "home.html"
+    form_class = NoteForm
+    success_url = '/'
 
+    def home(request):
+        notes = Note.objects.all()
+        form = NoteForm(request.POST or None)
+        if form.is_valid():
+            save_it = form.save(commit=False)
+            save_it.save(notes)
+
+
+        return render(request, 'home.html')
+
+"""
+
+"""display notes on note homepage"""
 class NoteIndex(generic.ListView):
+    model=Note
     queryset = models.Note.objects.all()
     template_name = "home.html"
-    paginate_by = 4
+    paginate_by = 5
 
+
+"""displaying the details of a selected note"""
 class NoteDetail(generic.DetailView):
     model = models.Note
     template_name =  "details.html"
