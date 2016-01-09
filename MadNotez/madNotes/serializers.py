b6ca6c8caa1fd0ed0e5a502960ed750c4e4c2647
b3h3rkz <barjebernard@gmail.com>
2016-01-09 05:31:43 +0000

    created a serializer for Note Model

--- MadNotez/madNotes/serializers.py
+++ MadNotez/madNotes/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
-from .models import No
+from .models import Note
+
+
+class NoteModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Note
+        fields = ("id","title", "created", "note")
